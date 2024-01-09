1ef0c73c29282c3e6dde5aa076361b32ba325d85
Clare Liguori <liguori@amazon.com>
2024-01-09 15:06:48 -0800

    Refactor to re-use invoke chain in other demos

--- stacks/blog_post_stack.py
+++ stacks/blog_post_stack.py
@@ -1,76 +1,11 @@
 from aws_cdk import (
     Duration,
     Stack,
-    aws_bedrock as bedrock,
     aws_stepfunctions as sfn,
-    aws_stepfunctions_tasks as tasks,
 )
 from constructs import Construct
 
-import builtins
-import typing
-
-from .util import add_bedrock_retries
-
-CLAUDE_HUMAN_PROMPT = """\n\nHuman:"""
-CLAUDE_AI_PROMPT = """\n\nAssistant:"""
-
-
-def get_claude_instant_invoke_chain(
-    scope: Construct,
-    id: builtins.str,
-    prompt: builtins.str,
-    max_tokens_to_sample: typing.Optional[int] = 250,
-    temperature: typing.Optional[float] = 1,
-    include_previous_conversation_in_prompt=True,
-):
-    model_prompt = sfn.JsonPath.format(
-        f"{CLAUDE_HUMAN_PROMPT}{{}}{CLAUDE_AI_PROMPT}",
-        prompt,
-    )
-    if include_previous_conversation_in_prompt:
-        model_prompt = sfn.JsonPath.format(
-            "{}{}",
-            sfn.JsonPath.string_at("$.output.conversation"),
-            model_prompt,
-        )
-    format_prompt = sfn.Pass(
-        scope,
-        id + " (Format Model Inputs)",
-        parameters={
-            "prompt": model_prompt,
-            "max_tokens_to_sample": max_tokens_to_sample,
-            "temperature": temperature,
-        },
-        result_path="$.model_inputs",
-    )
-    invoke_model = tasks.BedrockInvokeModel(
-        scope,
-        id + " (Invoke Model)",
-        model=bedrock.FoundationModel.from_foundation_model_id(
-            scope,
-            "Model",
-            bedrock.FoundationModelIdentifier.ANTHROPIC_CLAUDE_INSTANT_V1,
-        ),
-        body=sfn.TaskInput.from_json_path_at("$.model_inputs"),
-        result_selector={"response": sfn.JsonPath.string_at("$.Body.completion")},
-        result_path="$.model_outputs",
-    )
-    add_bedrock_retries(invoke_model)
-    format_response = sfn.Pass(
-        scope,
-        id + " (Format Model Outputs)",
-        parameters={
-            "response": sfn.JsonPath.string_at("$.model_outputs.response"),
-            "conversation": sfn.JsonPath.format(
-                "{}{}",
-                sfn.JsonPath.string_at("$.model_inputs.prompt"),
-                sfn.JsonPath.string_at("$.model_outputs.response"),
-            ),
-        },
-        result_path="$.output",
-    )
-    return format_prompt.next(invoke_model).next(format_response)
+from .util import get_claude_instant_invoke_chain
 
 
 class BlogPostStack(Stack):
@@ -136,9 +71,6 @@ class BlogPostStack(Stack):
         select_final_answer = sfn.Pass(
             scope,
             "Select Final Answer",
-            # parameters={
-            #     "final_answer": sfn.JsonPath.string_at("$.output.response"),
-            # },
             output_path="$.output.response",
         )
 
--- stacks/util.py
+++ stacks/util.py
@@ -1,10 +1,18 @@
 from aws_cdk import (
     Duration,
+    aws_bedrock as bedrock,
     aws_iam as iam,
     aws_lambda_python_alpha as lambda_python,
     aws_stepfunctions as sfn,
+    aws_stepfunctions_tasks as tasks,
 )
+from constructs import Construct
+import builtins
+import typing
 import jsii
+
+CLAUDE_HUMAN_PROMPT = """\n\nHuman:"""
+CLAUDE_AI_PROMPT = """\n\nAssistant:"""
 
 
 @jsii.implements(lambda_python.ICommandHooks)
@@ -48,3 +56,60 @@ def add_bedrock_retries(task):
         interval=Duration.seconds(5),
         max_delay=Duration.seconds(15),
     )
+
+
+def get_claude_instant_invoke_chain(
+    scope: Construct,
+    id: builtins.str,
+    prompt: builtins.str,
+    max_tokens_to_sample: typing.Optional[int] = 250,
+    temperature: typing.Optional[float] = 1,
+    include_previous_conversation_in_prompt=True,
+):
+    model_prompt = sfn.JsonPath.format(
+        f"{CLAUDE_HUMAN_PROMPT}{{}}{CLAUDE_AI_PROMPT}",
+        prompt,
+    )
+    if include_previous_conversation_in_prompt:
+        model_prompt = sfn.JsonPath.format(
+            "{}{}",
+            sfn.JsonPath.string_at("$.output.conversation"),
+            model_prompt,
+        )
+    format_prompt = sfn.Pass(
+        scope,
+        id + " (Format Model Inputs)",
+        parameters={
+            "prompt": model_prompt,
+            "max_tokens_to_sample": max_tokens_to_sample,
+            "temperature": temperature,
+        },
+        result_path="$.model_inputs",
+    )
+    invoke_model = tasks.BedrockInvokeModel(
+        scope,
+        id + " (Invoke Model)",
+        model=bedrock.FoundationModel.from_foundation_model_id(
+            scope,
+            "Model",
+            bedrock.FoundationModelIdentifier.ANTHROPIC_CLAUDE_INSTANT_V1,
+        ),
+        body=sfn.TaskInput.from_json_path_at("$.model_inputs"),
+        result_selector={"response": sfn.JsonPath.string_at("$.Body.completion")},
+        result_path="$.model_outputs",
+    )
+    add_bedrock_retries(invoke_model)
+    format_response = sfn.Pass(
+        scope,
+        id + " (Format Model Outputs)",
+        parameters={
+            "response": sfn.JsonPath.string_at("$.model_outputs.response"),
+            "conversation": sfn.JsonPath.format(
+                "{}{}",
+                sfn.JsonPath.string_at("$.model_inputs.prompt"),
+                sfn.JsonPath.string_at("$.model_outputs.response"),
+            ),
+        },
+        result_path="$.output",
+    )
+    return format_prompt.next(invoke_model).next(format_response)
