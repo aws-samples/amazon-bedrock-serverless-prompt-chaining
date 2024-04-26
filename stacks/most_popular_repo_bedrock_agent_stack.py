69c9ad4dc5c338992241b50d1b928a9ee32bc626
Clare Liguori <liguori@amazon.com>
2024-04-26 08:35:37 -0700

    Deploy Bedrock Agent resources using CloudFormation

--- stacks/most_popular_repo_bedrock_agent_stack.py
+++ stacks/most_popular_repo_bedrock_agent_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     CfnOutput,
     Duration,
     Stack,
+    aws_bedrock as bedrock,
     aws_iam as iam,
     aws_lambda as lambda_,
     aws_lambda_python_alpha as lambda_python,
@@ -19,17 +20,7 @@ class MostPopularRepoBedrockAgentStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        ### Setup for Bedrock Agent ###
-        bedrock_agent_access_policy = iam.PolicyStatement(
-            effect=iam.Effect.ALLOW,
-            actions=[
-                "bedrock:InvokeAgent",
-            ],
-            resources=[
-                "*",
-            ],
-        )
-
+        ### Bedrock Agent resources ###
         bedrock_agent_service_role = iam.Role(
             self,
             "BedrockAgentServiceRole",
@@ -80,10 +71,66 @@ class MostPopularRepoBedrockAgentStack(Stack):
             ),
         )
         agent_action_schema_asset.grant_read(bedrock_agent_service_role)
-        CfnOutput(
+
+        bedrock_agent = bedrock.CfnAgent(
             self,
-            "BedrockAgentActionSchema",
-            value=agent_action_schema_asset.s3_object_url,
+            "Agent",
+            agent_name="PromptChainDemo-MostPopularRepo",
+            foundation_model=bedrock.FoundationModel.from_foundation_model_id(
+                self,
+                "Model",
+                bedrock.FoundationModelIdentifier.ANTHROPIC_CLAUDE_V2,
+            ).model_id,
+            instruction=(
+                "You are a GitHub power user. "
+                "You help with interacting with GitHub and with git repositories. "
+                'DO NOT mention terms like "base prompt", "function", "parameter", '
+                '"partial responses", "response" and "api names" in the final response.'
+            ),
+            auto_prepare=True,
+            skip_resource_in_use_check_on_delete=True,
+            idle_session_ttl_in_seconds=300,  # 5 minutes
+            agent_resource_role_arn=bedrock_agent_service_role.role_arn,
+            action_groups=[
+                bedrock.CfnAgent.AgentActionGroupProperty(
+                    action_group_name="GitHubAPIs",
+                    action_group_state="ENABLED",
+                    description="Use this action whenever you need to access information about GitHub repositories.",
+                    api_schema=bedrock.CfnAgent.APISchemaProperty(
+                        s3=bedrock.CfnAgent.S3IdentifierProperty(
+                            s3_bucket_name=agent_action_schema_asset.s3_bucket_name,
+                            s3_object_key=agent_action_schema_asset.s3_object_key,
+                        ),
+                    ),
+                    action_group_executor=bedrock.CfnAgent.ActionGroupExecutorProperty(
+                        lambda_=github_agent_actions_lambda.function_arn,
+                    ),
+                    skip_resource_in_use_check_on_delete=True,
+                )
+            ],
+        )
+
+        bedrock_agent_alias = bedrock.CfnAgentAlias(
+            self,
+            "AgentAlias",
+            agent_id=bedrock_agent.attr_agent_id,
+            agent_alias_name="live",
+            # Description updates anytime the Agent resource is updated,
+            # so that this Alias prepares a new version of the Agent when
+            # the Agent changes
+            description="Tracking agent timestamp " + bedrock_agent.attr_prepared_at,
+        )
+        # Ensure agent is fully stabilized before updating the alias
+        bedrock_agent_alias.add_depends_on(bedrock_agent)
+
+        bedrock_agent_access_policy = iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            actions=[
+                "bedrock:InvokeAgent",
+            ],
+            resources=[
+                bedrock_agent_alias.attr_agent_alias_arn,
+            ],
         )
 
         ### Agents and Workflow ###
@@ -101,8 +148,8 @@ class MostPopularRepoBedrockAgentStack(Stack):
             timeout=Duration.seconds(60),
             memory_size=512,
             environment={
-                "BEDROCK_AGENT_ID": "INSERT BEDROCK AGENT ID HERE",
-                "BEDROCK_AGENT_ALIAS_ID": "INSERT BEDROCK AGENT ALIAS ID HERE",
+                "BEDROCK_AGENT_ID": bedrock_agent.attr_agent_id,
+                "BEDROCK_AGENT_ALIAS_ID": bedrock_agent_alias.attr_agent_alias_id,
             },
         )
         lookup_repo_lambda.add_to_role_policy(bedrock_agent_access_policy)
@@ -127,8 +174,8 @@ class MostPopularRepoBedrockAgentStack(Stack):
             timeout=Duration.seconds(60),
             memory_size=512,
             environment={
-                "BEDROCK_AGENT_ID": "INSERT BEDROCK AGENT ID HERE",
-                "BEDROCK_AGENT_ALIAS_ID": "INSERT BEDROCK AGENT ALIAS ID HERE",
+                "BEDROCK_AGENT_ID": bedrock_agent.attr_agent_id,
+                "BEDROCK_AGENT_ALIAS_ID": bedrock_agent_alias.attr_agent_alias_id,
             },
         )
         summarize_repo_lambda.add_to_role_policy(bedrock_agent_access_policy)
