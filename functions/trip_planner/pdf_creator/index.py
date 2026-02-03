08848a57ff5166632edfb43ad1a7578709056d9e
Clare Liguori <liguori@amazon.com>
2026-02-03 07:58:59 -0800

    Breaking changes in md2pdf

--- functions/trip_planner/pdf_creator/index.py
+++ functions/trip_planner/pdf_creator/index.py
@@ -20,18 +20,24 @@ Here is your three day itinerary for your visit to {location}, created by genera
 
     s3_object_key = f"itinerary-{uuid.uuid4()}.pdf"
 
-    with tempfile.NamedTemporaryFile(delete=True) as tmp:
+    with tempfile.NamedTemporaryFile(suffix='.pdf', delete=False) as pdf_tmp:
+        pdf_tmp_path = pdf_tmp.name
+
+    try:
         md2pdf(
-            tmp.name,
-            md_content=itinerary_content,
+            pdf_tmp_path,
+            raw=itinerary_content,
         )
 
         s3_client.upload_file(
-            tmp.name,
+            pdf_tmp_path,
             s3_bucket_name,
             s3_object_key,
             ExtraArgs={"ContentType": "application/pdf"},
         )
+    finally:
+        if os.path.exists(pdf_tmp_path):
+            os.unlink(pdf_tmp_path)
 
     url = s3_client.generate_presigned_url(
         "get_object",
